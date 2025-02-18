e0c8f94943e1a16531814fe23d3d6fc75bc10b77
049052 <049052@UNI-LAB2-34>
2025-02-18 20:10:02 -0300

    Exercicio04

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,13 @@
+﻿int num, resultado;
+
+Console.WriteLine("Digite um numero:");
+num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("O número é par!");
+}
+else
+{
+    Console.WriteLine("O número é impar!");
+}
