76fc2a5b072e2e0d31ea3ec87f8d71c346c9f5e5
049052 <049052@UNI-LAB2-34>
2025-02-18 20:01:50 -0300

    Exercicio 03

---
+++ Exercicio03/Program.cs
@@ -0,0 +1,13 @@
+﻿double nota1, nota2, nota3, media;
+
+Console.WriteLine("Digite a nota 1:");
+nota1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a nota 2:");
+nota2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a nota 3:");
+nota3 = double.Parse(Console.ReadLine());
+
+media = (((nota1 * 2) + (nota2 * 3) + (nota3 * 5)) / 3) ;
+Console.WriteLine("O valor da média é: " + media);
