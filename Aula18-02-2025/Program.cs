54dcc82045cad20b0f10cb6794ed19395389862a
049052 <049052@UNI-LAB2-34>
2025-02-18 19:49:16 -0300

    Exercico 02

---
+++ Aula18-02-2025/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Exercício 02");
+
+int A, B, C; 
+double R, S, O;
+
+Console.WriteLine("Escreva o valor de A:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Escreva o valor de B:");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Escreva o valor de C:");
+int c = int.Parse(Console.ReadLine());
+
+R = Math.Pow(a + b, 2);
+
+S = Math.Pow(b + c, 2);
+
+O = (R + S) / 2;
+
+Console.WriteLine(O);
+
+
